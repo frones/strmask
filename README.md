fa78f460b39215168577da14949bdac876475939
Gabriel Frones <gabriel@frones.com.br>
2019-11-23 19:51:43 -0300

    I changed a lot how the functions work and the symbols accepted.

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # strmask
 This package provides a simple string formatter function for go based on masks.
 
+The mask argument consists of three fields separated by semicolons: the mask, a pad character and a Right-to-Left specifier. Only the first field is required.
+
+## Options Fields
+
+### Pad character
+The pad character is the character added to the result string when a required character class cannot be matched in the source string (notice that the unmatched character in the source string is not skipped). If no pad character is specified, a space is used.
+
+### Right-to-Left specifier
+If RTL processing is requested ('1'), the mask and the source string are reversed before processing, and the output is reversed afterwards. This causes extra characters in the source string to be at the left of the masked text and if the mask is bigger than the source string, the extra required characters in the mask are also padded to left.
+
 # Usage
 ```go
-fmt.Printf("%s", strmask.FormatMask("00.000.000/0000-00", "12520501000188"))
+fmt.Printf("%s", strmask.FormatMask("00.000.000/0000-00;0;1", "12520501000188")) // Pad with zeroes, process RTL
 ```
 Outputs `12.520.501/0001-88`
 
@@ -17,16 +27,19 @@ Outputs `OL -4508, invalid character "4" (expected a letter) at position 2`
 Symbol | Meaning
 --- | ---
 `\` | Escapes next character
+`>` | Maps all letters that follow the symbol to upper case until the end of the mask or until a resetting `=` appears
+`<` | Maps all letters that follow the symbol to lower case until the end of the mask or until a resetting `=` appears
+`=` | Resets the case modifiers (`>` and `<`)
 `0` | Requires a decimal digit at this position
 `9` | Permits a decimal digit at this position, but doesn't require it
-`L` | Requires an unicode letter at this position
-`l` | Permits an unicode letter at this position, but doesn't require it
-`A` | Requires a decimal digit or unicode letter at this position
-`a` | Permits a decimal digit or unicode letter at this position, but doesn't require it
-`U` | Requires an unicode letter at this position and maps it to upper case
-`u` | Permits an unicode letter at this position, but doesn't require it, and maps it to upper case
-`W` | Requires an unicode letter at this position and maps it to lower case
-`w` | Permits an unicode letter at this position, but doesn't require it, and maps it to lower case
+`L` | Requires an ascii letter at this position
+`l` | Permits an ascii letter at this position, but doesn't require it
+`A` | Requires a decimal digit or ascii letter at this position
+`a` | Permits a decimal digit or ascii letter at this position, but doesn't require it
+`W` | Requires an unicode letter at this position
+`w` | Permits an unicode letter at this position, but doesn't require it
+`C` | Requires an arbitrary character at this position
+`c` | Permits an arbitrary character at this position, but doesn't require it
 
 # Acknowledgement
-I took some inspiration from https://github.com/the-darc/string-mask and Delphi's `FormatMaskText`, but my requirements were so simple that I didn't bother reading their codes and just wrote it from scratch. I don't know if the output is compatible with any of these (or any other mask formatting library out there). I guess not, but it's good enough for what I need.
+I took some inspiration from Delphi's `FormatMaskText` and from https://github.com/the-darc/string-mask, but didn't bother reading their codes and just wrote it from scratch. I don't know if the output is compatible with any of these (or any other mask formatting library out there). I guess not, but it's good enough for what I need.
--- strmask.go
+++ strmask.go
@@ -3,24 +3,73 @@ package strmask
 
 import (
 	"fmt"
+	"strings"
 	"unicode"
 	"unicode/utf8"
 )
 
+var asciiLetters = &unicode.RangeTable{
+	R16: []unicode.Range16{
+		{'A', 'Z', 1},
+		{'a', 'z', 1},
+	},
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func reverseBackslashs(s string) string {
+	runes := []rune(s)
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == '\\' && i > 0 {
+			runes[i-1], runes[i] = runes[i], runes[i-1]
+		}
+	}
+	return string(runes)
+}
+
 // ValidateAndFormatMask formats the string s based on the mask, reporting any invalid runes that doesn't fit the provided mask. Information about the mask symbols can be found at: https://github.com/frones/strmask
-func ValidateAndFormatMask(mask string, s string) (string, error) {
+func ValidateAndFormatMask(mask string, str string) (string, error) {
+	padChar := ' '
+	rtl := false
+
+	args := strings.Split(mask, ";")
+	for _, s := range strings.Split(mask, ";")[1:] {
+		if r, _ := utf8.DecodeLastRuneInString(args[0]); r == '\\' {
+			args[0] += ";" + s
+			args = append(args[:1], args[2:]...)
+		}
+	}
+	mask = args[0]
+	if len(args) >= 2 {
+		padChar, _ = utf8.DecodeRuneInString(args[1])
+	}
+	if len(args) >= 3 {
+		rtl = args[2] == "1"
+	}
+
+	if rtl {
+		mask = reverseBackslashs(reverse(mask))
+		str = reverse(str)
+	}
+
 	printNext := false
 	output := ""
 	strOffset := 0
 	lastErrOffset := -1
 	err := ""
+	charCase := 'N'
 	for _, r := range mask {
 		if printNext {
 			output += string(r)
 			printNext = false
 
-			r2, w := utf8.DecodeRuneInString(s[strOffset:])
-			if r2 == r {
+			if r2, w := utf8.DecodeRuneInString(str[strOffset:]); r2 == r {
 				strOffset += w
 			}
 
@@ -30,8 +79,14 @@ func ValidateAndFormatMask(mask string, s string) (string, error) {
 		switch r {
 		case '\\':
 			printNext = true
+		case '>':
+			charCase = 'U'
+		case '<':
+			charCase = 'L'
+		case '=':
+			charCase = 'N'
 		case '9', '0':
-			r2, w := utf8.DecodeRuneInString(s[strOffset:])
+			r2, w := utf8.DecodeRuneInString(str[strOffset:])
 			if unicode.IsDigit(r2) {
 				output += string(r2)
 				strOffset += w
@@ -40,67 +95,87 @@ func ValidateAndFormatMask(mask string, s string) (string, error) {
 					err += fmt.Sprintf("invalid character \"%s\" (expected a digit) at position %d\n", string(r2), strOffset)
 					lastErrOffset = strOffset
 				}
-				output += " "
+				output += string(padChar)
 			}
 		case 'L', 'l':
-			r2, w := utf8.DecodeRuneInString(s[strOffset:])
-			if unicode.IsLetter(r2) {
-				output += string(r2)
+			r2, w := utf8.DecodeRuneInString(str[strOffset:])
+			if unicode.In(r2, asciiLetters) {
+				if charCase == 'U' {
+					output += string(unicode.ToUpper(r2))
+				} else if charCase == 'L' {
+					output += string(unicode.ToLower(r2))
+				} else {
+					output += string(r2)
+				}
 				strOffset += w
 			} else if r == 'L' {
 				if lastErrOffset < strOffset {
-					err += fmt.Sprintf("invalid character \"%s\" (expected a letter) at position %d\n", string(r2), strOffset)
+					err += fmt.Sprintf("invalid character \"%s\" (expected an ascii letter) at position %d\n", string(r2), strOffset)
 					lastErrOffset = strOffset
 				}
-				output += " "
+				output += string(padChar)
 			}
 		case 'A', 'a':
-			r2, w := utf8.DecodeRuneInString(s[strOffset:])
-			if unicode.IsLetter(r2) || unicode.IsDigit(r2) {
-				output += string(r2)
+			r2, w := utf8.DecodeRuneInString(str[strOffset:])
+			if unicode.In(r2, asciiLetters) || unicode.IsDigit(r2) {
+				if charCase == 'U' {
+					output += string(unicode.ToUpper(r2))
+				} else if charCase == 'L' {
+					output += string(unicode.ToLower(r2))
+				} else {
+					output += string(r2)
+				}
 				strOffset += w
 			} else if r == 'A' {
 				if lastErrOffset < strOffset {
-					err += fmt.Sprintf("invalid character \"%s\" (expected a letter or digit) at position %d\n", string(r2), strOffset)
+					err += fmt.Sprintf("invalid character \"%s\" (expected an ascii letter or a digit) at position %d\n", string(r2), strOffset)
 					lastErrOffset = strOffset
 				}
-				output += " "
-			}
-		case 'U', 'u':
-			r2, w := utf8.DecodeRuneInString(s[strOffset:])
-			if unicode.IsLetter(r2) {
-				output += string(unicode.ToUpper(r2))
-				strOffset += w
-			} else if r == 'U' {
-				if lastErrOffset < strOffset {
-					err += fmt.Sprintf("invalid character \"%s\" (expected a letter) at position %d\n", string(r2), strOffset)
-					lastErrOffset = strOffset
-				}
-				output += " "
+				output += string(padChar)
 			}
 		case 'W', 'w':
-			r2, w := utf8.DecodeRuneInString(s[strOffset:])
+			r2, w := utf8.DecodeRuneInString(str[strOffset:])
 			if unicode.IsLetter(r2) {
-				output += string(unicode.ToLower(r2))
+				if charCase == 'U' {
+					output += string(unicode.ToUpper(r2))
+				} else if charCase == 'L' {
+					output += string(unicode.ToLower(r2))
+				} else {
+					output += string(r2)
+				}
 				strOffset += w
 			} else if r == 'W' {
 				if lastErrOffset < strOffset {
-					err += fmt.Sprintf("invalid character \"%s\" (expected a letter) at position %d\n", string(r2), strOffset)
+					err += fmt.Sprintf("invalid character \"%s\" (expected an unicode letter) at position %d\n", string(r2), strOffset)
 					lastErrOffset = strOffset
 				}
-				output += " "
+				output += string(padChar)
 			}
+		case 'C', 'c':
+			r2, w := utf8.DecodeRuneInString(str[strOffset:])
+			if charCase == 'U' {
+				output += string(unicode.ToUpper(r2))
+			} else if charCase == 'L' {
+				output += string(unicode.ToLower(r2))
+			} else {
+				output += string(r2)
+			}
+			strOffset += w
 		default:
 			output += string(r)
-			r2, w := utf8.DecodeRuneInString(s[strOffset:])
+			r2, w := utf8.DecodeRuneInString(str[strOffset:])
 			if r2 == r {
 				strOffset += w
 			}
 		}
 	}
 
-	if len(s) > strOffset {
-		output += s[strOffset:]
+	if len(str) > strOffset {
+		output += str[strOffset:]
+	}
+
+	if rtl {
+		output = reverse(output)
 	}
 
 	if err != "" {
